ce73ea6f7548c791c6f4dbbbb2536a2d6d196924
Andrés Enríquez <fabio.and1514@gmail.com>
2020-06-04 12:27:58 -0500

    Estructura del proyecto

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/Auth/login/login.component';
+import { MainScreenComponent } from './components/layouts/main-screen/main-screen.component';
+import { EstudiantesComponent } from './components/estudiantes/estudiantes.component';
+import { AutoridadesComponent } from './components/autoridades/autoridades.component';
+import { RecursosComponent } from './components/recursos/recursos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'main', component: MainScreenComponent, children: [
+    { path: 'estudiantes', component: EstudiantesComponent },
+    { path: 'autoridades', component: AutoridadesComponent },
+    { path: 'recursos', component: RecursosComponent },
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EstudiantesComponent } from './components/estudiantes/estudiantes.component';
+import { AutoridadesComponent } from './components/autoridades/autoridades.component';
+import { RecursosComponent } from './components/recursos/recursos.component';
+import { LoginComponent } from './components/Auth/login/login.component';
+import { RegisterComponent } from './components/Auth/register/register.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { MainScreenComponent } from './components/layouts/main-screen/main-screen.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstudiantesComponent,
+    AutoridadesComponent,
+    RecursosComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    MainScreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
