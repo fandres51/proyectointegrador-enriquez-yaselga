94967b878583059c33c73c83a445916daebf1a4e
Andrés Enríquez <fabio.and1514@gmail.com>
2020-06-16 12:32:42 -0500

    filtros funcionando

--- front-end/src/app/components/estudiantes/filtros-estudiantes/filtros-estudiantes.component.ts
+++ front-end/src/app/components/estudiantes/filtros-estudiantes/filtros-estudiantes.component.ts
@@ -23,56 +23,64 @@ export class FiltrosEstudiantesComponent implements OnInit {
   public filtroSemestre: string = "";
   public filtroGenero: string = "";
   public filtroCarrera: string = "";
+  public tipoOrdenamiento: string = "";
 
   constructor() { }
 
   ngOnInit(): void { }
 
+  /***Búsqueda****************************************** */
+  
   public buscarPorNombre() {
     this.estudiantesMostrados = this.estudiantes.filter(n => {
       return n.Nombre.search(this.porNombreInput) != -1 || n.Apellido.search(this.porNombreInput) != -1;
     })
     this.enviarEstudiantes();
   }
-
+  
   public buscarPorCedula() {
     this.estudiantesMostrados = this.estudiantes.filter(n => {
       return n.id.search(this.porCedulaInput) != -1;
     })
     this.enviarEstudiantes();
   }
-
+  
+  /***Filtros****************************************** */
+  
   public filtroGeneroFun(valor: string) {
     this.filtroGenero = valor;
     this.filtrar();
   }
-
+  
   public filtroCarreraFun(valor: string) {
     this.filtroCarrera = valor;
+    console.log(this.filtroCarrera);
     this.filtrar();
   }
-
+  
   public filtroSmestreFun(valor: string) {
     this.filtroSemestre = valor;
     this.filtrar();
   }
-
+  
   public filtroAfiliadoFun(valor: string) {
     this.filtroAfiliado = valor;
     this.filtrar();
   }
-
+  
   public filtrar() {
     this.estudiantesMostrados = this.estudiantes.filter(n => {
-      if (this.filtroAfiliado == "true") {
-        return n.EstadoAfiliacion && (n.Genero.search(this.filtroGenero) + 1) && (n.SemestreReferencial.search(this.filtroSemestre) + 1) && (n.Carrera.search(this.filtroCarrera) + 1);
-      } else {
+      if (this.filtroAfiliado == '') {
         return (n.Genero.search(this.filtroGenero) + 1) && (n.SemestreReferencial.search(this.filtroSemestre) + 1) && (n.Carrera.search(this.filtroCarrera) + 1);
+      } else {
+        return ((this.filtroAfiliado == 'true') == n.EstadoAfiliacion) && (n.Genero.search(this.filtroGenero) + 1) && (n.SemestreReferencial.search(this.filtroSemestre) + 1) && (n.Carrera.search(this.filtroCarrera) + 1);
       }
     })
     this.enviarEstudiantes();
   }
-
+  
+  /***Ordenamiento*******************************************/
+  
   compararPorApellido(a, b) {
     if (a.Apellido < b.Apellido) {
       return -1;
@@ -82,7 +90,7 @@ export class FiltrosEstudiantesComponent implements OnInit {
     }
     return 0;
   }
-
+  
   compararPorSemestre(a, b) {
     if (a.SemestreReferencial < b.SemestreReferencial) {
       return -1;
@@ -92,7 +100,7 @@ export class FiltrosEstudiantesComponent implements OnInit {
     }
     return 0;
   }
-
+  
   compararPorCarrera(a, b) {
     if (a.Carrera < b.Carrera) {
       return -1;
@@ -102,13 +110,13 @@ export class FiltrosEstudiantesComponent implements OnInit {
     }
     return 0;
   }
-
+  
   ordenar(tipo: string) {
+    this.tipoOrdenamiento = tipo;
     if (this.estudiantesMostrados != []) {
       if (tipo == 'apellido') {
         this.estudiantes.sort(this.compararPorApellido);
-      } else if (tipo == 'numero') {
-        console.log('Hola');
+      } else if (tipo == 'carrera') {
         this.estudiantes.sort(this.compararPorCarrera);
       } else {
         this.estudiantes.sort(this.compararPorSemestre);
@@ -116,7 +124,7 @@ export class FiltrosEstudiantesComponent implements OnInit {
     } else {
       if (tipo == 'apellido') {
         this.estudiantesMostrados.sort(this.compararPorApellido);
-      } else if (tipo == 'numero') {
+      } else if (tipo == 'carrera') {
         this.estudiantesMostrados.sort(this.compararPorCarrera);
       } else {
         this.estudiantesMostrados.sort(this.compararPorSemestre);
@@ -124,7 +132,9 @@ export class FiltrosEstudiantesComponent implements OnInit {
     }
     this.enviarEstudiantes;
   }
-
+  
+  /***Enviar****************************************** */
+  
   public enviarEstudiantes() {
     this.eventEmitter.emit(this.estudiantesMostrados);
   }
