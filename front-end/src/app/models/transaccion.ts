0e24176285ff454adf1bd62abe88b035f4a9f708
Andrés Enríquez <fabio.and1514@gmail.com>
2020-06-22 22:35:36 -0500

    modelo y servicio de transacciones

---
+++ front-end/src/app/models/transaccion.ts
@@ -0,0 +1,11 @@
+export interface Transaccion {
+    id:string;
+    Descripcion?:string;
+    EventoID:string;
+    Fecha:Date;
+    FilialID:String;
+    Monto:number;
+    PersonaID:string;
+    Tipo:string;
+    TipoMonetario:string;
+}
