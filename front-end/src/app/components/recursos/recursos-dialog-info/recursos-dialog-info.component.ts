8c49f43edee3906f50057961fc9076d31d9c1c58
Ernesteins <eyaselga@hotmail.com>
2020-10-01 20:31:52 -0500

    correccion de EventEmitter

--- front-end/src/app/components/recursos/recursos-dialog-info/recursos-dialog-info.component.ts
+++ front-end/src/app/components/recursos/recursos-dialog-info/recursos-dialog-info.component.ts
@@ -1,6 +1,6 @@
-import { Component, Inject, OnInit, Output } from '@angular/core';
+import { Component, Inject, OnInit, Output ,EventEmitter} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { EventEmitter } from 'protractor';
+
 import { Recurso } from 'src/app/models/recurso';
 import { RecursosService } from 'src/app/services/recursos.service';
 
