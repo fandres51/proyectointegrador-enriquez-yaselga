ae6da6479f4cd5baa3c3ff06ed9d4ebaa9c27b5f
Andrés Enríquez <fabio.and1514@gmail.com>
2020-11-10 12:18:44 -0500

    cambios a los permisos y el css

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -17,15 +17,15 @@ export const environment = {
   } */
   ///SITIO DE CIVIL
   firebase: {
-    apiKey: "AIzaSyBDvm-wm2VOKxS8JRe8nadViI4wpuC_7AE",
-      authDomain: "asociaciones-2.firebaseapp.com",
-      databaseURL: "https://asociaciones-2.firebaseio.com/",
-      projectId: "asociaciones-2",
-      storageBucket: "asociaciones-2.appspot.com",
-      messagingSenderId: "705889851394",
-      appId: "1:705889851394:web:3e758fbcd8c3a4b93def1b",
-      measurementId: "G-QRZLCXZE8T"
-    }
+    apiKey: "AIzaSyCLGuNMMHI46Kd6zEd9m0Ls-U6QTjUvwuw",
+    authDomain: "asociaciones-5b17b.firebaseapp.com",
+    databaseURL: "https://asociaciones-5b17b.firebaseio.com",
+    projectId: "asociaciones-5b17b",
+    storageBucket: "asociaciones-5b17b.appspot.com",
+    messagingSenderId: "906143785356",
+    appId: "1:906143785356:web:ebcdb07abeeaf7bd4d76a9",
+    measurementId: "G-4Z1TK5XZF0"
+  }
 };
 
 /*
