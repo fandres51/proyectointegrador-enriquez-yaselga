9d33e3deb0ea35eb7cf117c66b482146ffd7d3db
Ernesteins <eyaselga@hotmail.com>
2020-11-27 01:45:32 -0500

    Edicion filiales
    Finanzas y Alquileres aun por finalizar en filial

--- front-end/src/app/components/filiales/filiales-editar/filiales-editar.component.ts
+++ front-end/src/app/components/filiales/filiales-editar/filiales-editar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Filial } from 'src/app/models/filial';
+import { FilialService } from 'src/app/services/filial.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-filiales-editar',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilialesEditarComponent implements OnInit {
 
-  constructor() { }
+  filial: Filial;
+
+  constructor(
+    private filialService:FilialService,
+    private route:ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.filial={
+      id:'',
+      nombre:'',
+      descripcion:'',
+      ubicacion:''
+    }
+    this.route.params.subscribe(params =>{
+      const noUnicoParam = params['id'];
+      this.filialService.getFilial(noUnicoParam).subscribe(
+        recurso=>{
+          if(recurso!=null){
+            this.filial=recurso;
+          }else{
+            console.log("Error: no fue posible cargar la filial")
+          }
+        },
+        error =>{
+          console.error(error);
+        }
+      )
+    },
+    error=>{
+      console.error(error);
+    }
+    );
+  }
+
+  guardar(){
+    
+    this.filialService.updateFilial(this.filial);
+    this.router.navigateByUrl('/filiales')
   }
 
 }
